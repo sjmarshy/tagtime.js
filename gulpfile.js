bfba0bb0067371136efdadb0d74c3e8a6e548ad4
Sam Marshall <sjmarshy@gmail.com>
2014-07-27 22:15:41 +0100

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var cs   = require('gulp-coffee');
 
+var cffeeSrc = './coffee/**/*.coffee';
+
 gulp.task('default', function () {
-    gulp.src('./coffee/**/*.coffee')
+    gulp.src(cffeeSrc)
         .pipe(cs())
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(cffeeSrc, ['default']);
 });
