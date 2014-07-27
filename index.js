f406ff6f302c42c543bdd1a36aae05edaf9bcb02
Sam Marshall <sjmarshy@gmail.com>
2014-07-27 22:15:29 +0100

    add produced index

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 (function() {
-  var Pings, fs;
+  var Pings, fs, isFile, os, path, spawn, touch, write;
 
   fs = require('fs');
 
+  os = require('os');
+
+  path = require('path');
+
   Pings = require('./src/pings');
+
+  spawn = require('child_process').spawn;
+
+  touch = function(filename, done) {
+    return fs.open(filename, 'w', function(error, fd) {
+      if (error) {
+        return done(new Error('unable to create tmpfile'));
+      } else {
+        return fs.close(fd, done);
+      }
+    });
+  };
+
+  isFile = function(name) {
+    return fs.existsSync(name);
+  };
+
+  write = function(name, data, done) {
+    return fs.writeFile(name, data, function(error) {
+      if (error) {
+        return done(error);
+      } else {
+        return done();
+      }
+    });
+  };
 
   fs.readFile('./config/tagtime.json', function(error, buffer) {
     var config, pinger;
@@ -13,7 +43,53 @@
     } else {
       config = JSON.parse(buffer.toString());
       pinger = new Pings(config.frequency);
-      return pinger.start();
+      pinger.start();
+      return pinger.on('ping', function(now) {
+        var tmpfile;
+        tmpfile = path.join(os.tmpdir(), "ping-" + now);
+        return touch(tmpfile, function(error) {
+          var gvim;
+          if (error) {
+            console.error(error);
+            return process.exit(1);
+          } else {
+            gvim = spawn('gvim', ['-f', '--', tmpfile]);
+            return gvim.on('close', function() {
+              return fs.readFile(tmpfile, function(error, data) {
+                var delTmpfile, log, writeLog;
+                log = './log.json';
+                delTmpfile = function() {
+                  return fs.unlink(tmpfile);
+                };
+                writeLog = function() {
+                  return fs.readFile(log, function(error, logdata) {
+                    var logJSON, newData;
+                    if (!error) {
+                      if (logdata.length > 0) {
+                        logJSON = JSON.parse(logdata.toString());
+                      } else {
+                        logJSON = {};
+                      }
+                      logJSON[now] = data.toString();
+                      newData = JSON.stringify(logJSON);
+                      return write(log, newData, function() {
+                        return delTmpfile();
+                      });
+                    }
+                  });
+                };
+                if (isFile(log)) {
+                  return writeLog();
+                } else {
+                  return touch(log, function() {
+                    return writeLog();
+                  });
+                }
+              });
+            });
+          }
+        });
+      });
     }
   });
 
