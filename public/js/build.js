7b10c063bf19232a7e3576ed3c8597d0ef9dcf14
Sam Marshall <sjmarshy@gmail.com>
2014-08-25 20:47:30 +0100

    refactor(index): rewrite front-end display

--- public/js/build.js
+++ public/js/build.js
@@ -56,101 +56,174 @@ var calculateYPosition = function (tag, i, sortedTags, headTagHeight, padding) {
     return "translate(0," + heightModifier  + ")";
 };
 
+/**
+ * The svg elements will appear as follows:
+ * <g class="tag"><!-- a single top-level tag group and it's children -->
+ *      <g height="{{60 * tag.depth}}" class="{{tag.name}}">
+ *          <rect width="{{100%}} height="60">
+ *          </rect>
+ *          <text>
+ *              {{tag.name}}
+ *          </text>
+ *      </g>
+ *      {{#tag.children}}
+ *          <g
+ *              class="{{child.name}}
+ *              height="{{60 * child.depth}}
+ *              x={{60 * tag.depth - child.depth}}
+ *              y={{tag.children.slice(0, i).width}} // work out children before it
+ *          >
+ *          </g>
+ *      {{/tag.children}}
+ * </g>
+ */
+
 $.get('/api/tag/top', function(data) {
     var sortedTags    = reverseArrayDestructive(_.sortBy(data, 'count'));
     var numberOfTags  = sortedTags.length;
     var padding       = 10;
     var headTagHeight = 60;
-    var tagWidth      = $('body').width() - 20;
+    var tagGroupWidth = $('body').width() - 20;
 
     var page = d3.select('svg');
 
-    var childScale = d3.scale.linear();
+    var childMargin = {left: 1, right:1};
+    var childScale  = d3.scale.linear();
 
-    childScale.range([0, tagWidth]);
+    childScale.range([0, tagGroupWidth]);
 
-    var tag = page.selectAll('g')
-        .data(sortedTags).enter()
-        .append('g')
-        .classed('tag', true)
-        .attr('height', function (tag) {
-            return headTagHeight * tag.depth;
-        })
-        .attr('width', tagWidth)
-        .attr('transform', function (tag, i) {
-            return calculateYPosition(tag, i, sortedTags, headTagHeight, padding);
-        });
+    var appendGroup = function (selection) {
+        return selection.append('g');
+    };
 
-    tag.append('rect')
-        .attr('width', tagWidth)
-        .attr('height', function () {
-            return headTagHeight;
-        })
-        .attr('title', function (tag) {
-            return tag.count;
-        })
-        .style('fill', function () {
-            return '#' + hexColorsLoop();
-        });
+    var addNewChildGroup = function (tagGroup) {
+        var childTagGroups = tagGroup.selectAll('g')
+            .data(function (d) {
+                if (d.childCount > 0) {
+                    return _.map(d.children, function (child) {
+                        child.parent = d;
+                        return child;
+                    });
+                } else {
+                    return {};
+                }
 
-    tag.append('text')
-        .attr('y', 15)
-        .attr('x', 10)
-        .style('fill', 'white')
-        .text(getTagName);
+            }).enter().append('g');
 
-    tag.append('g').classed('child', true)
-        .attr('width', tagWidth);
-
-
-    var child = tag.selectAll('.child').selectAll('rect')
-        .data(function(d) {
-            if (d.childCount > 0) {
-                var children = d.children;
-                var totalCount = _.chain(children).map(function (tag) {
-                    return tag.count;
-                }).reduce(function (memo, tagCount) {
-                    return memo + tagCount;
-                }).value();
-
-                var getChildWidth = function (count) {
-                    childScale.domain([0, totalCount]);
-                    return childScale(count);
-                };
-
-                var previousChildrenWidth = function (i) {
-                    return _.reduce(children.slice(0, i), function (memo, child) {
-                        return memo + getChildWidth(child.count);
+        childTagGroups
+            .attr('class', getTagName)
+            .attr('width', function (child) { // children
+                if (!child.width) {
+                    var totalSiblingCount = _
+                    .reduce(child.parent.children, function (memo, c) {
+                        return c.count + memo;
                     }, 0);
-                };
+                    if (child.parent && child.parent.width) {
+                        childScale.range([0, child.parent.width]);
+                    }
+                    childScale.domain([0, totalSiblingCount]);
+                    child.width = childScale(child.count);
+                }
+                return child.width;
+            })
+            .attr('transform', function (child, index) {
+                if (!child.x || !child.y) {
+                    var totalSiblingCount = _
+                    .reduce(child.parent.children, function (memo, c) {
+                        return c.count + memo;
+                    }, 0);
+                    childScale.domain([0, totalSiblingCount]);
 
-                return _.map(children, function (tag, i) {
-                    return {
-                        tag: tag,
-                        width: getChildWidth(tag.count),
-                        left: previousChildrenWidth(i),
-                        totalCount: totalCount
-                    };
-                });
-            } else {
-                return {};
-            }
-        }).enter().append('rect');
+                    var x = _.reduce(child.parent.children.slice(0, index),
+                        function (memo, c) {
+                            return memo + c.count;
+                        },
+                        0);
 
-    child
+                    var y = (child.parent.depth * headTagHeight) -
+                        ((child.parent.depth - 1) * headTagHeight);
+
+                    child.x = childScale(x);
+                    child.y = y;
+                }
+                return 'translate(' + child.x + ',' + child.y + ')';
+            });
+
+        var childTagRects = childTagGroups.append('rect')
+            .attr('height', headTagHeight)
+            .attr('width', function (child) { // children
+                console.log(child);
+                if (!child.width) {
+                    var totalSiblingCount = _
+                    .reduce(child.parent.children, function (memo, c) {
+                        return c.count + memo;
+                    }, 0);
+                    childScale.domain([0, totalSiblingCount]);
+                    child.width = childScale(child.count);
+                }
+                return child.width;
+            })
+            .attr('title', function (d) {
+                return d.tag + ', ' + d.count;
+            })
+            .style('fill', function () {
+                return '#' + hexColorsLoop();
+            });
+
+        return childTagGroups;
+    };
+
+    var topLevelTagGroup = page.selectAll('g') // set the selection to 'groups'
+        .data(sortedTags)                      // feed it the array of top-level tags
+        .enter().append('g');                  // create array of grp objects per tag
+
+    topLevelTagGroup
+        .attr('width', tagGroupWidth)
+        .attr('height', function (d) {
+            return d.depth * headTagHeight;
+        })
+        .attr('class', getTagName)
+        .attr('transform', function (d, i) {
+            return 'translate(0,' +
+                heightSoFar(sortedTags.slice(0, i), headTagHeight, padding) + ')';
+        });
+
+    var topLevelRectangle = topLevelTagGroup
+        .append('rect')
+        .attr('width', tagGroupWidth)
         .attr('height', headTagHeight)
-        .attr('width', function (d) {
-            return d.width;
-        })
-        .attr('transform', function (d) {
-            return 'translate(' + d.left + ',' + (headTagHeight + 2) + ')';
-        })
-        .attr('title', function (d) {
-            return d.tag.tag + ', ' + d.tag.count;
-        })
         .style('fill', function () {
             return '#' + hexColorsLoop();
         });
+
+    var childTagGroups = addNewChildGroup(
+        appendGroup(topLevelTagGroup).attr('width', tagGroupWidth)
+            .attr('height', function (d) {
+                return (d.depth - 1) * headTagHeight;
+            })
+    );
+
+    var childTagGroups2 = addNewChildGroup(
+        appendGroup(childTagGroups).attr('width', tagGroupWidth)
+            .attr('height', function (d) {
+                return (d.depth - 1) * headTagHeight;
+            })
+    );
+
+    var childTagGroups3 = addNewChildGroup(
+            appendGroup(childTagGroups2).attr('width', tagGroupWidth)
+                .attr('height', function (d) {
+                    return (d.depth - 1) * headTagHeight;
+                })
+            );
+
+    var childTagGroups4 = addNewChildGroup(
+            appendGroup(childTagGroups3).attr('width', tagGroupWidth)
+                .attr('height', function (d) {
+                    return (d.depth - 1) * headTagHeight;
+                })
+            );
+
 
     $('body').height(function () {
         return heightSoFar(sortedTags, headTagHeight, padding);
